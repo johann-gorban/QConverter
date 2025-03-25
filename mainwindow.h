459197196258676b1f0ae9ff4f2941360dec0f77
der-johanni <ivangorbanwork@gmail.com>
2025-03-25 10:28:08 +0300

    ui files deleted

--- mainwindow.h
+++ mainwindow.h
@@ -1,23 +1,20 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#pragme once
 
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
+
 namespace Ui {
-class MainWindow;
+    class MainWindow;
 }
+
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
 
+class MainWindow : public QMainWindow {
+    Q_OBJECT
 public:
     MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
 
-private:
-    Ui::MainWindow *ui;
+    ~MainWindow();
 };
-#endif // MAINWINDOW_H
